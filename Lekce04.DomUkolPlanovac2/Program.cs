ba048a3e02bbb02ca972deea21b1ea37ddee4917
HanaJelen <HSokolikova@seznam.cz>
2025-05-11 22:11:15 +0200

    add StringToDate method

--- Lekce04.DomUkolPlanovac2/Program.cs
+++ Lekce04.DomUkolPlanovac2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Specialized;
 using System.Globalization;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 
 public class Event
 {
@@ -13,8 +14,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        List<Event> ListOfEvents = new List<Event>();
-        Dictionary<DateTime, int> Statistics = new Dictionary<DateTime, int>();
+        List<Event> listOfEvents = new List<Event>();
+        Dictionary<DateTime, int> statistics = new Dictionary<DateTime, int>();
 
         while (true)
         {
@@ -27,35 +28,25 @@ internal class Program
                 {
                 case "EVENT":
                     string name = prvkyVstupu[1];
-                    string[] casti = prvkyVstupu[2].Split("-");
-
-                    
-                    string date = prvkyVstupu[2];
-                    DateTime dateValue;
-
-                    while(DateTime.TryParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue) == false)
-                    {
-                        Console.WriteLine("Date format is not correct. Use yyyy-MM-dd format.");
-                        date = Console.ReadLine();
-                    }
+                    DateTime dateValue = StringToDate(prvkyVstupu[2]);
 
                     Event udalost = new Event(){Name = name, Date = dateValue};
-                    ListOfEvents.Add(udalost);//fill list of objects
+                    listOfEvents.Add(udalost);//fill list of objects
 
                     //fill dictionary with statistics 
-                    if (Statistics.ContainsKey(dateValue))
+                    if (statistics.ContainsKey(dateValue))
                     {
-                        Statistics[dateValue] += 1;
+                        statistics[dateValue] += 1;
                     }
                     else
                     {
-                        Statistics.Add(dateValue, 1);
+                        statistics.Add(dateValue, 1);
                     }                  
                     break;
 
                 case "LIST":
                     DateTime actualTime = DateTime.Now;
-                    foreach (var item in ListOfEvents.OrderBy(item => item.Date))
+                    foreach (var item in listOfEvents.OrderBy(item => item.Date))
                     {
                         int difference = (item.Date - actualTime).Days;
                         string dateFormat = item.Date.ToString();
@@ -64,7 +55,7 @@ internal class Program
                     }
                     break;
                 case "STATS":
-                    foreach (var item in Statistics)
+                    foreach (var item in statistics)
 		            {
                         string dateFormat = item.Key.ToString();
                         dateFormat = $"{dateFormat.Substring(6,4)}-{dateFormat.Substring(3,2)}-{dateFormat.Substring(0,2)}";
@@ -79,5 +70,21 @@ internal class Program
                 }
             
         }
+    }
+    public static DateTime StringToDate(string date)
+    {
+        DateTime dateValue;
+        while(DateTime.TryParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue) == false)
+            {
+                Console.WriteLine("Date format is not correct. Use yyyy-MM-dd format.");
+                date = LoadInput();
+            }
+        return dateValue;
+    }
+
+    public static string LoadInput()
+    {
+        string vstup = Console.ReadLine();
+        return vstup;
     }
 }
