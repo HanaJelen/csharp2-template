a60ac8f629ec4aff9546d4ec870b26a068fa83ca
HanaJelen <HSokolikova@seznam.cz>
2025-05-19 15:53:07 +0200

    initial commit

---
+++ Lekce06.DomUkolKnihovna/Book.cs
@@ -0,0 +1,83 @@
+using System;
+using System.IO.Compression;
+
+namespace Lekce06.DomUkolKnihovna;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public DateTime PublishedDate { get; set; }
+    private int pages;
+    public int Pages
+    {
+        get { return pages; }//validace počtu stránek, kdy počet stran nemůže být nulový, nula znamená nezmámou hodnotu
+        set
+        {
+            if (value < 0)
+            {
+                pages = 0;
+            }
+            else
+            {
+                pages = value;
+            }
+        }
+    }
+    public static List<Book> ListOfBooks = [];
+    public static Dictionary<DateTime, int> Statistics = [];
+
+    //constructor
+    public Book(string title, string author, DateTime publishedDate, int pages)
+    {
+        Title = title;
+        Author = author;
+        PublishedDate = publishedDate;
+        Pages = pages;
+    }
+    //metoda ADD
+    public static void AddToList(string title, string author, DateTime publishedDate, int pages)
+    {
+        ListOfBooks.Add(new Book(title, author, publishedDate, pages));
+    }
+    //metoda LIST
+    public static void GetListOfBooks()
+    {
+    foreach (var item in ListOfBooks.OrderByDescending(item => item.PublishedDate))
+        {
+            Console.WriteLine($"Kniha: {item.Title}, autor: {item.Author}, vydáno: {item.PublishedDate.ToString("d.M.yyyy")}, stran: {item.Pages}");
+        }
+    }
+    //metoda STATS
+    public static void GetStatistics()
+    {
+        var averagePageNumber = ListOfBooks.Average(z => z.Pages);//průměrný počet stran na knihu v knihovně
+
+        Console.WriteLine($"Průměrný počet stran: {averagePageNumber:F0}");
+
+        Console.WriteLine($"Počet knih dle autora:");//přiřazení počtu knih v knihovně ke kontrontrétnímu autorovi
+        foreach (var group in ListOfBooks.GroupBy(a => a.Author))
+        {
+            var authors = group.Select(a => a.Author);
+            Console.WriteLine($" -  {group.Key}: {group.Count()}");
+        }
+
+        var distinctWorlds = ListOfBooks.SelectMany(z => z.Title.Split(" ")).Distinct().Count();//unikátní slova obsažena v titulcích knih
+        Console.WriteLine($"Počet unikátních slov obsažených v názvech knih je: {distinctWorlds}");
+
+    }
+    //metoda FIND
+    public static void GetTitle(string world)
+    {
+        Console.WriteLine($"""Výsledky hledání pro "{world}":""");
+        foreach (var item in ListOfBooks)
+        {
+            if (item.Title.ToLower().Contains(world.ToLower()))
+            {
+                Console.WriteLine($" - {item.Title}");
+            }
+        }
+        var vyber = from v in ListOfBooks where v.Title.Contains(world) select v.Title.ToList();
+    }
+
+}
---
+++ Lekce06.DomUkolKnihovna/Program.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Specialized;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace Lekce06.DomUkolKnihovna;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Book.ListOfBooks = new List<Book>();
+        Book.Statistics = new Dictionary<DateTime, int>();
+
+        while (true)
+        {
+            Console.WriteLine("Use format ADD;[název];[autor];[datum vydání ve formátu YYYY-MM-DD];[počet stran]:");
+
+            string[] prvkyVstupu = LoadInputToParts();
+
+            switch (prvkyVstupu[0].ToUpper().Trim())
+            {
+                case "ADD":
+                    Book.AddToList(prvkyVstupu[1].Trim(), prvkyVstupu[2].Trim(), StringToDate(prvkyVstupu[3].Trim()), StringToNumber(prvkyVstupu[4].Trim()));
+                    break;
+
+                case "LIST":
+                    Book.GetListOfBooks();
+                    break;
+                case "STATS":
+                    Book.GetStatistics();
+                    break;
+                case "FIND":
+                    Book.GetTitle(prvkyVstupu[1]);
+                    break;
+                case "END":
+                    return;
+                default:
+                    Console.WriteLine("Format is not correct. Check that text contains ADD, LIST, STATS or FIND. Support format: ADD;[název];[autor];[datum vydání ve formátu YYYY-MM-DD];[počet stran]");
+                    break;
+            }
+
+        }
+    }
+
+    public static DateTime StringToDate(string date)//parsování datového typu
+    {
+        DateTime dateValue;
+        while (DateTime.TryParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue) == false)
+        {
+            Console.WriteLine("Date format is not correct. Use yyyy-MM-dd format.");
+            date = LoadInput();
+        }
+        return dateValue;
+    }
+
+    public static int StringToNumber(string number)//parsování vstup obsahujících celočíselnou hodnotu
+    {
+        int pageNumber;
+        while (int.TryParse(number, out pageNumber) == false)
+        {
+            Console.WriteLine("Pages must be integer.");
+            number = LoadInput();
+        }
+        return pageNumber;
+    }
+
+    public static string[] LoadInputToParts()//metoda pro rozdělení načteného vstupu na jednotlivé části, v případě ADD/FIND kontroluje, zda zadání bylo kompletní
+    {
+        string input = LoadInput();
+        string[] inputParts = input.Split(";");
+        if (inputParts[0] == "ADD")
+        {
+            while (inputParts.Length != 5)
+            {
+                Console.WriteLine($"You not enter all paramets. Try it again in format ADD;[název];[autor];[datum vydání ve formátu YYYY-MM-DD];[počet stran].");
+                input = LoadInput();
+                inputParts = input.Split(";");
+            }
+            return inputParts;
+        }
+        if (inputParts[0] == "FIND")
+        {
+            while (inputParts.Length != 2)
+            {
+                Console.WriteLine($"You not enter all paramets. Try it again in format FIND;[world].");
+                input = LoadInput();
+                inputParts = input.Split(";");
+            }
+            return inputParts;
+        }
+        else
+        {
+            return inputParts;
+        }
+    }
+
+    public static string LoadInput()//načtení a ověření vstupu, že se nejedná o prázdný řetězec či řetezec s hodnotou null
+    {
+        string input = Console.ReadLine();
+        while (string.IsNullOrWhiteSpace(input) == true)
+        {
+            Console.WriteLine("Please enter some input.");
+            input = Console.ReadLine();
+        }
+        return input;
+    }
+}
